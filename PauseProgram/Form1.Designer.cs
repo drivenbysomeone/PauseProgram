1eb0f104d7a008a89a49d219f5464081fc15ff03
drivenbysomeone <aspit1020@ASPITPC16.tietgen.dk>
2015-05-26 10:49:51 +0200

    Design complete
    This is only the design view version - no other code implementation has
    been added.

---
+++ PauseProgram/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace PauseProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBoxLocation = new System.Windows.Forms.TextBox();
+            this.txtBoxTime = new System.Windows.Forms.TextBox();
+            this.cmbBoxSchedule = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnStartCountdown = new System.Windows.Forms.Button();
+            this.btnStopCountdown = new System.Windows.Forms.Button();
+            this.lblBreakTime = new System.Windows.Forms.Label();
+            this.lblTimeTillBreak = new System.Windows.Forms.Label();
+            this.lblLocationChoice = new System.Windows.Forms.Label();
+            this.lblLocationInfo = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(500, 377);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblLocationChoice);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(492, 351);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Vis tider";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblTimeTillBreak);
+            this.tabPage2.Controls.Add(this.lblBreakTime);
+            this.tabPage2.Controls.Add(this.btnStopCountdown);
+            this.tabPage2.Controls.Add(this.btnStartCountdown);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.btnUpdate);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(492, 351);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Start alarmer";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(192, 260);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(102, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblText);
+            this.groupBox1.Controls.Add(this.lblTime);
+            this.groupBox1.Controls.Add(this.lblLocationInfo);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.cmbBoxSchedule);
+            this.groupBox1.Controls.Add(this.txtBoxTime);
+            this.groupBox1.Controls.Add(this.txtBoxLocation);
+            this.groupBox1.Location = new System.Drawing.Point(244, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 260);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rediger tider";
+            // 
+            // txtBoxLocation
+            // 
+            this.txtBoxLocation.Location = new System.Drawing.Point(93, 72);
+            this.txtBoxLocation.Name = "txtBoxLocation";
+            this.txtBoxLocation.ReadOnly = true;
+            this.txtBoxLocation.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxLocation.TabIndex = 0;
+            // 
+            // txtBoxTime
+            // 
+            this.txtBoxTime.Location = new System.Drawing.Point(93, 111);
+            this.txtBoxTime.Name = "txtBoxTime";
+            this.txtBoxTime.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxTime.TabIndex = 1;
+            // 
+            // cmbBoxSchedule
+            // 
+            this.cmbBoxSchedule.FormattingEnabled = true;
+            this.cmbBoxSchedule.Location = new System.Drawing.Point(93, 147);
+            this.cmbBoxSchedule.Name = "cmbBoxSchedule";
+            this.cmbBoxSchedule.Size = new System.Drawing.Size(100, 21);
+            this.cmbBoxSchedule.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(26, 195);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Gem";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(130, 195);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Slet";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(38, 127);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Opdater";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(240, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnStartCountdown
+            // 
+            this.btnStartCountdown.Location = new System.Drawing.Point(240, 127);
+            this.btnStartCountdown.Name = "btnStartCountdown";
+            this.btnStartCountdown.Size = new System.Drawing.Size(75, 23);
+            this.btnStartCountdown.TabIndex = 3;
+            this.btnStartCountdown.Text = "Start nedtælling";
+            this.btnStartCountdown.UseVisualStyleBackColor = true;
+            // 
+            // btnStopCountdown
+            // 
+            this.btnStopCountdown.Location = new System.Drawing.Point(240, 179);
+            this.btnStopCountdown.Name = "btnStopCountdown";
+            this.btnStopCountdown.Size = new System.Drawing.Size(75, 23);
+            this.btnStopCountdown.TabIndex = 4;
+            this.btnStopCountdown.Text = "Stop nedtælling";
+            this.btnStopCountdown.UseVisualStyleBackColor = true;
+            // 
+            // lblBreakTime
+            // 
+            this.lblBreakTime.AutoSize = true;
+            this.lblBreakTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBreakTime.Location = new System.Drawing.Point(35, 36);
+            this.lblBreakTime.Name = "lblBreakTime";
+            this.lblBreakTime.Size = new System.Drawing.Size(77, 20);
+            this.lblBreakTime.TabIndex = 5;
+            this.lblBreakTime.Text = "Pause kl.";
+            // 
+            // lblTimeTillBreak
+            // 
+            this.lblTimeTillBreak.AutoSize = true;
+            this.lblTimeTillBreak.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeTillBreak.Location = new System.Drawing.Point(237, 36);
+            this.lblTimeTillBreak.Name = "lblTimeTillBreak";
+            this.lblTimeTillBreak.Size = new System.Drawing.Size(93, 20);
+            this.lblTimeTillBreak.TabIndex = 6;
+            this.lblTimeTillBreak.Text = "Starter om:";
+            // 
+            // lblLocationChoice
+            // 
+            this.lblLocationChoice.AutoSize = true;
+            this.lblLocationChoice.Location = new System.Drawing.Point(28, 19);
+            this.lblLocationChoice.Name = "lblLocationChoice";
+            this.lblLocationChoice.Size = new System.Drawing.Size(51, 13);
+            this.lblLocationChoice.TabIndex = 3;
+            this.lblLocationChoice.Text = "Lokation:";
+            // 
+            // lblLocationInfo
+            // 
+            this.lblLocationInfo.AutoSize = true;
+            this.lblLocationInfo.Location = new System.Drawing.Point(23, 72);
+            this.lblLocationInfo.Name = "lblLocationInfo";
+            this.lblLocationInfo.Size = new System.Drawing.Size(51, 13);
+            this.lblLocationInfo.TabIndex = 4;
+            this.lblLocationInfo.Text = "Lokation:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(23, 118);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(25, 13);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "Tid:";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(23, 150);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(37, 13);
+            this.lblText.TabIndex = 6;
+            this.lblText.Text = "Tekst:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 377);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label lblLocationChoice;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblLocationInfo;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cmbBoxSchedule;
+        private System.Windows.Forms.TextBox txtBoxTime;
+        private System.Windows.Forms.TextBox txtBoxLocation;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblTimeTillBreak;
+        private System.Windows.Forms.Label lblBreakTime;
+        private System.Windows.Forms.Button btnStopCountdown;
+        private System.Windows.Forms.Button btnStartCountdown;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
